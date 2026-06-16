1563cac15bd29fad650e6c11d125486719918763
VKJAIN2006 <vivekjain844080@gmail.com>
2026-06-16 08:12:42 +0530

    dotenv modified to ue toml case

--- db.py
+++ db.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("host"),
-        user=os.getenv("user"),
-        password=os.getenv("password"),
-        database=os.getenv("database")
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE")
     )
 
